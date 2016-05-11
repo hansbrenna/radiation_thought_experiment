04ade5e7879746371191e81ac3b04c9677461b54
hansbrenna <hans.brenna@geo.uio.no>
2016-05-11 12:20:28 +0200

    initial commit

---
+++ radiation.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('PS')
+import matplotlib.pyplot as plt
+
+"""Back radiation thought experiment illustrative simulation"""
+
+sb = 5.67e-8 #Stephan-Boltzmann constant
+S = 100 #Constant energy input term
+c = 3e8 #speed of light
+n = 1000
+dz = n*c #Distance between plates (n light seconds)
+dt = 1 #time step, seconds
+cap = 1 #heat capacity
+
+eii = 0.5
+Ti0 = 0 #K
+Tii0 = 0 #K
+z = np.linspace(0,dz,n) #Discretized space
+
+jiz = np.zeros(z.shape)
+jiiz = np.zeros(z.shape)
+
+t0 = 0
+ts = 10*n
+time = np.arange(t0,ts,dt)
+
+ji = np.zeros(time.shape)
+jii = np.zeros(time.shape)
+Ti = np.zeros(time.shape)
+Tii = np.zeros(time.shape)
+Su = np.zeros(time.shape)
+
+for i in xrange(1,int(ts)):
+    if i == ts/2:
+        eii = 0.8
+    Ti[i] =1.0/cap*(S+jiiz[0]-ji[i-1])*dt
+    ji[i] = sb*Ti[i]**4
+    jiz[1:]=jiz[0:n-1]
+    jiz[0] = ji[i]
+    Tii[i] =1.0/cap*(eii*jiz[n-1]-2*jii[i-1])*dt
+    jii[i] = eii*sb*Tii[i]**4
+    jiiz[0:n-1]=jiiz[1:n]
+    jiiz[n-1] = jii[i]
+    Su[i] = (1-eii*jiz[n-1])+jii[i]
+
+fig = plt.figure()
+plt.plot(time,Ti,time,Tii)
+
+fig.savefig('temperatures.png')
+
+
+
+
+
+
